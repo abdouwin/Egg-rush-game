54cb9456cd30df25b7df1a47b62f3ce5cbca6b15
abdouwin <85121117+abdouwin@users.noreply.github.com>
2021-12-28 01:19:22 -0800

    Fx
    fx

---
+++ Eggs Rush/Assets/Scripts/wateranimator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wateranimator : MonoBehaviour
+ {
+	public float speedX = 0.5f;
+    public float speedY = 0.5f;
+    private float curX;
+    private float curY;
+ 
+    // Update is called once per frame
+    void Update () {
+        curX = Time.time * speedX;
+        curY = Time.time * speedY;
+        GetComponent<Renderer>().material.mainTextureOffset = new Vector2(curX, curY);
+    }
+
+}
