00525b1fc1d14646d990139347e950102be7b28d
abdouwin <85121117+abdouwin@users.noreply.github.com>
2021-12-27 23:40:30 -0800

    road + some animations test
    i'm working now Good morning bro

---
+++ Eggs Rush/Assets/Scripts/ro.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ro : MonoBehaviour
+{
+  
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0,0,5f);
+    }
+}
